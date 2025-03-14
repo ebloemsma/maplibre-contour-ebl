96ec72dc32776ecdbdde9a91c46d99e0396a41ab
ebloemsma <eike@bloemsma.de>
2025-03-14 18:49:27 +0100

    ts config: allow any and create source maps

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 import terser from "@rollup/plugin-terser";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import sourcemaps from 'rollup-plugin-sourcemaps';
 
 export const nodeResolve = resolve({
   browser: true,
@@ -26,6 +27,7 @@ export default [
   {
     input: ["src/index.ts", "src/worker.ts"],
     output: {
+      sourcemap: true,
       dir: "dist/staging",
       format: "amd",
       indent: false,
@@ -37,7 +39,7 @@ export default [
       throw message;
     },
     treeshake: true,
-    plugins: [nodeResolve, typescript(), commonjs()],
+    plugins: [nodeResolve, typescript({ sourceMap: true, inlineSources: true }), commonjs(),sourcemaps(),],
   },
   create("dist/index.cjs", "cjs"),
   create("dist/index.mjs", "esm"),
