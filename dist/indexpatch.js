99da50a47eae265856982b3f79608c3f48b0992d
ebloemsma <eike@bloemsma.de>
2025-04-15 14:32:09 +0200

    `ip-build`

--- dist/indexpatch.js
+++ dist/indexpatch.js
@@ -418,6 +418,19 @@
     function lineArrayToStrings(lines) {
         return lines.map(l => l.toString());
     }
+    function reverseLine(line) {
+        if (!line) {
+            throw new Error("Invalid input: line null");
+        }
+        if (line.length % 2 !== 0) {
+            throw new Error("Invalid input: line must have even number of coordinates");
+        }
+        const reversed = [];
+        for (let i = line.length - 2; i >= 0; i -= 2) {
+            reversed.push(line[i], line[i + 1]);
+        }
+        return reversed;
+    }
 
     class TileInformation {
         setTile(tile) {
@@ -751,6 +764,10 @@
         }
         clone() {
             const line = new TiledLine(this.line, this.minXY, this.maxXY);
+            return line;
+        }
+        cloneReversed() {
+            const line = new TiledLine(reverseLine(this.line), this.minXY, this.maxXY);
             return line;
         }
         get hash() {
@@ -1372,13 +1389,13 @@
         // SET-DBG convertTileIsolinesToPolygons 
         const dbg = Number(`${1}`);
         // const dbg = Number(`${tileInfo.isTile(null, 11, 21) ? "1" : "0"}`);
-        // if( tileInfo.isTile(null, 37, 84)  ){
-        //     const line11CloseWithCorners = new TiledLine([2000,-32,  2000,400,  1000,400,  1000,-32 ],-32,4128)
-        //     const line11DirectCloseable = new TiledLine( reverseLine(line11CloseWithCorners.line),-32,4128)
-        //     const closed = LineIndex.closeLine(line11DirectCloseable,-32,4128)
-        //     console.log("## FAKE",closed.toString(),closed)
-        //     return [closed.line]
-        // }
+        if (tileInfo.isTile(null, 37, 84)) {
+            const line11CloseWithCorners = new TiledLine([2000, -32, 2000, 400, 1000, 400, 1000, -32], -32, 4128);
+            const line11DirectCloseable = line11CloseWithCorners.cloneReversed();
+            const closed = LineIndex.closeLine(line11DirectCloseable, -32, 4128);
+            console.log("## FAKE", closed.toString(), closed);
+            return [closed.line];
+        }
         // filter out lines that are not valid, too small
         const lines = linesIn.filter(l => l.length >= 6);
         if (!lines || lines.length < 1) {
